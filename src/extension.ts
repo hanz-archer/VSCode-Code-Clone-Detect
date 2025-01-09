da1958317f5c0513eaba205a4493293ba8174a98
Hans Archer Montezon Dalubatan <dalubatanhans@gmail.com>
2025-01-09 19:19:08 +0800

    files

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,16 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { detectCodeClones } from './cloneDetection';
+import { highlightDuplicates } from './highlight';
+import { integrateGit } from './gitIntegration';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    const cloneCommand = vscode.commands.registerCommand('extension.detectClones', async () => {
+        const files = await vscode.workspace.findFiles('**/*.ts');
+        const clones = detectCodeClones(files);
+        highlightDuplicates(clones);
+    });
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "code-clone" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('code-clone.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Code Clone!');
-	});
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(cloneCommand);
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate (){}
